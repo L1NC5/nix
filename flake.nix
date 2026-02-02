ae624fdcfc19076c2ed9d822d6c4958d336331d9
Federico Barbieri <barbieri.federico96@gmail.com>
2026-02-02 20:40:27 +0100

    feat: add import-tree, initial dendritic host config

--- flake.nix
+++ flake.nix
@@ -4,48 +4,38 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nvf = {
       url = "github:NotAShelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    nixpkgs,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
-
-      flake = {
-        nixosConfigurations = {
-          alchemist = nixpkgs.lib.nixosSystem {
-            specialArgs = {inherit inputs;};
-            modules = [
-              ./system/hosts/alchemist
-              inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
-              inputs.home-manager.nixosModules.home-manager
-            ];
-          };
-        };
-      };
-    };
+  outputs = inputs
+  :
+    inputs.flake-parts.lib.mkFlake {inherit inputs;}
+    (inputs.import-tree ./modules);
 }
---
+++ modules/hosts/alchemist.nix
@@ -0,0 +1,100 @@
+{
+  inputs,
+  self,
+  ...
+}: {
+  systems = ["x86_64-linux"];
+
+  flake.nixosConfigurations.alchemist = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.alchemistModule
+      inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
+      # system specific:
+      # configure audio interface's LEDs when loading is complete
+      ({pkgs, ...}: {
+        services.udev.extraRules = ''
+          SUBSYSTEM=="sound", ACTION=="add", KERNEL=="controlC*", RUN+="${pkgs.bash}/bin/bash -c 'sleep 1; echo follow-route > /sys/class/sound/ctl-led/mic/mode; echo off > /sys/class/sound/ctl-led/speaker/mode'"
+        '';
+      })
+    ];
+  };
+
+  flake.nixosModules.alchemistModule = {pkgs, ...}: let
+    username = "l1nc5";
+  in {
+    imports = [
+      inputs.home-manager.nixosModules.home-manager
+
+      "${self}/system/hosts/alchemist/hardware-configuration.nix"
+
+      "${self}/system/modules/nix-settings.nix"
+      "${self}/system/modules/hardware/bluetooth.nix"
+      "${self}/system/modules/hardware/graphics.nix"
+      "${self}/system/modules/hardware/laptop.nix"
+      "${self}/system/modules/hardware/laptop-lid.nix"
+
+      "${self}/system/roles/desktop.nix"
+    ];
+
+    networking.hostName = "alchemist";
+    networking.networkmanager.enable = true;
+    time.timeZone = "Europe/Rome";
+    i18n.defaultLocale = "it_IT.UTF-8";
+
+    nixpkgs.config.allowUnfree = true;
+
+    boot.loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    hardware.cpu.amd.updateMicrocode = true;
+
+    services = {
+      keyd = {
+        enable = true;
+        keyboards.default.settings = {
+          main = {
+            capslock = "overload(control,esc)";
+            esc = "capslock";
+          };
+        };
+      };
+
+      sabnzbd = {
+        enable = true;
+        openFirewall = true;
+        user = username;
+        group = "users";
+      };
+    };
+
+    users.users.${username} = {
+      isNormalUser = true;
+      description = username;
+      extraGroups = [
+        "networkmanager"
+        "sabnzbd"
+        "wheel"
+      ];
+    };
+
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      extraSpecialArgs = {inherit inputs;};
+      users.${username} = import "${inputs.self}/home/profiles/${username}/home.nix";
+    };
+
+    programs = {
+      niri.enable = true;
+    };
+
+    # this value determines the nixos release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. it‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "25.11"; # did you read the comment?
+  };
+}
