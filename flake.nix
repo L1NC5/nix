54efe57e71e7bded89276fd69370677f240d7c53
Federico Barbieri <barbieri.federico96@gmail.com>
2026-02-18 01:23:55 +0100

    feat: move hosts outside module folder

--- flake.nix
+++ flake.nix
@@ -34,8 +34,12 @@
     };
   };
 
-  outputs = inputs
-  :
-    inputs.flake-parts.lib.mkFlake {inherit inputs;}
-    (inputs.import-tree ./modules);
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        (inputs.import-tree ./hosts)
+        (inputs.import-tree ./modules)
+      ];
+    };
 }
