a1c4bdc088b2d19ccb47337bfbd216802df2ce4b
L1NC5 <barbieri.federico96@gmail.com>
2026-02-27 19:37:36 +0100

    feat(zen): remove bonjourr-startpage

--- modules/browsers/zen.nix
+++ modules/browsers/zen.nix
@@ -33,7 +33,6 @@
           extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
             ublock-origin
             sponsorblock
-            bonjourr-startpage
             react-devtools
           ];
 
