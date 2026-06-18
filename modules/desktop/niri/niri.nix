7a2ddce83a22ed7acb73dc74f0e184bf79ee8bee
Federico Barbieri <barbieri.federico96@gmail.com>
2026-06-18 14:02:07 +0200

    fix(desktop): update niri's bindings

--- modules/desktop/niri/niri.nix
+++ modules/desktop/niri/niri.nix
@@ -1,15 +1,18 @@
 {
   flake.modules = {
-    nixos.niri = {pkgs, ...}: {
+    nixos.niri = { pkgs, ... }: {
       environment.systemPackages = [
         pkgs.niri
       ];
       programs.niri.enable = true;
     };
-    homeManager.niri = {config, ...}: let
-      configPath = "${config.home.homeDirectory}/nix/modules/desktop/niri";
-    in {
-      xdg.configFile."niri".source = config.lib.file.mkOutOfStoreSymlink configPath;
-    };
+    homeManager.niri =
+      { config, ... }:
+      let
+        configPath = "${config.home.homeDirectory}/nix/modules/desktop/niri";
+      in
+      {
+        xdg.configFile."niri".source = config.lib.file.mkOutOfStoreSymlink configPath;
+      };
   };
 }
