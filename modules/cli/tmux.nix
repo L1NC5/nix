c5c294c7f6b1b3880c481436b150ba3d4f0f87bd
Federico Barbieri <barbieri.federico96@gmail.com>
2026-06-18 14:02:17 +0200

    feat(cli): create tmux module

--- modules/cli/tmux.nix
+++ modules/cli/tmux.nix
@@ -8,12 +8,28 @@
       programs = {
         tmux = {
           enable = true;
-          keyMode = "vi";
+          keyMode = "emacs";
           mouse = true;
           prefix = "C-a";
           shell = "${pkgs.zsh}/bin/zsh";
           clock24 = true;
           disableConfirmationPrompt = true;
+          sensibleOnTop = true;
+          baseIndex = 1;
+          focusEvents = true;
+          extraConfig = ''
+            bind C-y display-popup \
+            -d "#{pane_current_path}" \
+            -w 80% \
+            -h 80% \
+            -E "${pkgs.lazygit}/bin/lazygit"
+
+            bind C-f display-popup \
+            -d "#{pane_current_path}" \
+            -w 90% \
+            -h 90% \
+            -E "${pkgs.superfile}/bin/superfile"
+          '';
           plugins = with pkgs; [
             tmuxPlugins.cpu
             {
