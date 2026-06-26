e24aac4ecd5a79322cdf3183a58b8a6f6d29c44b
Federico Barbieri <barbieri.federico96@gmail.com>
2026-06-26 09:58:14 +0200

    fix: set correct noctalia cache

--- flake.nix
+++ flake.nix
@@ -1,36 +1,38 @@
 {
   description = "l1nc5's nix-config";
 
+  nixConfig = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    noctalia.url = "github:noctalia-dev/noctalia/cachix";
     nvf = {
       url = "github:NotAShelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    cole-nvim = {
+      url = "github:thekylehuang/cole.nvim";
+      flake = false;
     };
   };
 
@@ -41,15 +43,22 @@
         (inputs.import-tree ./hosts)
         (inputs.import-tree ./modules)
       ];
-
       perSystem = { pkgs, ... }: {
         packages.neovim =
           (inputs.nvf.lib.neovimConfiguration {
             inherit pkgs;
-            modules = [ ./nvf/nvf-config.nix ];
+            extraSpecialArgs = { inherit inputs; };
+            modules = [
+              ./nvf/nvf-config.nix
+              ({ ... }: {
+                config.vim.extraPlugins.cole.package = pkgs.vimUtils.buildVimPlugin {
+                  name = "cole";
+                  src = inputs.cole-nvim;
+                };
+              })
+            ];
           }).neovim;
       };
-
       _module.args = {
         inherit inputs;
         noctaliaModule = inputs.noctalia.homeModules.default;
--- hosts/nixos/alchemist/alchemist.nix
+++ hosts/nixos/alchemist/alchemist.nix
@@ -3,18 +3,20 @@
   self,
   ...
 }:
+let
+  flakePath = "path:${inputs.self.outPath}";
+in
 {
   flake.nixosConfigurations.alchemist = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit inputs self; };
     modules = with self.modules.nixos; [
       # Global modules
       system-laptop
-
       # Host specific modules
       hardware-alchemist
       host-alchemist
       alchemist-led
       inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
-
       # Home Manager
       home-base
       l1nc5
@@ -28,31 +30,18 @@
       networking.networkmanager.enable = true;
       time.timeZone = "Europe/Rome";
       i18n.defaultLocale = "en_US.UTF-8";
-
       nixpkgs.config.allowUnfree = true;
-
       boot.loader = {
         systemd-boot.enable = true;
         efi.canTouchEfiVariables = true;
       };
-
       environment.systemPackages = with pkgs; [ lmstudio ];
-
-      services = {
-        sabnzbd = {
-          enable = true;
-          openFirewall = true;
-          user = "l1nc5";
-          group = "users";
-        };
+      services.sabnzbd = {
+        enable = true;
+        openFirewall = true;
+        user = "l1nc5";
+        group = "users";
       };
-
-      # this value determines the nixos release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. it‘s perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "25.11"; # did you read the comment?
+      system.stateVersion = "25.11";
     };
 }
