2031145722c9d4f64797fbbe22381fe7bf88f9d3
Federico Barbieri <barbieri.federico96@gmail.com>
2026-07-05 11:24:56 +0200

    feat(cli): set ghostty theme

--- modules/cli/ghostty.nix
+++ modules/cli/ghostty.nix
@@ -10,7 +10,7 @@
           background-opacity = "0.9";
           background-blur = true;
           font-family = "Rec Mono Semicasual";
-          theme = "Ayu";
+          theme = "Vague";
         };
       };
     };
